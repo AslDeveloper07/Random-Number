a5759c325529beeddcb9cfa59aad63b934077ecf
AslDeveloper07 <aslwexking777@gmail.com>
2025-05-04 10:12:04 +0500

    add

---
+++ app.js
@@ -0,0 +1,8 @@
+let againButton = document.querySelector('.btn')
+let secretNumbers = document.querySelector('#secretNumber')
+let guessText = document.querySelector('.guess')
+let secretInput =document.querySelector('#secretInp')
+let checkButton = document.querySelector('.check')
+let scoreAvg = document.querySelector('.scoreBall')
+let winScoreAvg = document.querySelector('.winScore')
+console.log(againButton, secretNumbers, guessText, checkButton, scoreAvg, winScoreAvg,secretInput);
